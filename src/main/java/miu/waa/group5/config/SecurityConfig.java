3b51c0c97916ee0c377bdd51abfcc58dc8562e0e
mohammed-almassri <mohammed.m.almassri@gmail.com>
2025-03-18 17:39:59 -0500

    cors

--- src/main/java/miu/waa/group5/config/SecurityConfig.java
+++ src/main/java/miu/waa/group5/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Allow requests from React dev server
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Allow requests from React dev server
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials such as cookies
