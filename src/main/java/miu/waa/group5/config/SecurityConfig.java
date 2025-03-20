553d5aa2145a9192a4f5ce24d3be4f318f80863f
Daisuke-lab <kikuchidaisuke.zr@gmail.com>
2025-03-19 20:20:37 -0500

    PATCH ADDED

--- src/main/java/miu/waa/group5/config/SecurityConfig.java
+++ src/main/java/miu/waa/group5/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Allow requests from React dev server
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials such as cookies
         configuration.setMaxAge(3600L);
