2a806d560bd4c98307ff329bd4effc8f749b76e8
mohammed-almassri <mohammed.m.almassri@gmail.com>
2025-03-20 13:08:40 -0500

    added vercel to cors

--- src/main/java/miu/waa/group5/config/SecurityConfig.java
+++ src/main/java/miu/waa/group5/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Allow requests from React dev server
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://waa-final-project-fe.vercel.app")); // Allow requests from React dev server
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials such as cookies
