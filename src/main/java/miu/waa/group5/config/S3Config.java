2451273e0ed9d9b25c410413d422c6c504162f0f
mohammed-almassri <mohammed.m.almassri@gmail.com>
2025-03-20 13:13:59 -0500

    s3 config use instance storage on prod

--- src/main/java/miu/waa/group5/config/S3Config.java
+++ src/main/java/miu/waa/group5/config/S3Config.java
@@ -3,6 +3,8 @@ package miu.waa.group5.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -16,11 +18,38 @@ public class S3Config {
     @Value("${aws.s3.region}")
     private String region;
 
-    @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(ProfileCredentialsProvider.create(awsProfile))
-                .build();
+//    @Bean
+//    public S3Client s3Client() {
+//        return S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(ProfileCredentialsProvider.create(awsProfile))
+//                .build();
+//    }
+
+    @Configuration
+    @Profile("dev")
+    static class LocalConfig {
+        @Value("${aws.profile}")
+        private String awsProfile;
+
+        @Bean
+        public S3Client s3Client(S3Config s3Config) {
+            return S3Client.builder()
+                    .region(Region.of(s3Config.region))
+                    .credentialsProvider(ProfileCredentialsProvider.create(awsProfile))
+                    .build();
+        }
+    }
+
+    @Configuration
+    @Profile("!dev")
+    static class ProdConfig {
+        @Bean
+        public S3Client s3Client(S3Config s3Config) {
+            return S3Client.builder()
+                    .region(Region.of(s3Config.region))
+                    .credentialsProvider(InstanceProfileCredentialsProvider.create())
+                    .build();
+        }
     }
 }
